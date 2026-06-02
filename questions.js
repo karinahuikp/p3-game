d54594821e7047db342aa3e440c24cd8eb9aa99f
karinahuikp <karina.huikp@gmail.com>
2026-06-02 17:19:39 +0800

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,14 @@
+const questions=[
+  {sentence:'今天下雨了___我們取消了旅行。',answer:['，']},
+  {sentence:'小美喜歡唱歌___跳舞和畫畫。',answer:['、']},
+  {sentence:'你今天有沒有做功課___',answer:['？']},
+  {sentence:'哇___這朵花真漂亮___',answer:['！','！']},
+  {sentence:'小明心想___我一定要努力溫習。',answer:['：']},
+  {sentence:'媽媽說___「吃飯前要洗手。」',answer:['：']},
+  {sentence:'森林裏有兔子___松鼠和小鳥。',answer:['、']},
+  {sentence:'放學後___我們一起去圖書館看書。',answer:['，']},
+  {sentence:'小狗不停地叫___真吵啊___',answer:['，','！']},
+  {sentence:'你為甚麼哭___',answer:['？']},
+  {sentence:'哥哥大聲說___「我們成功了___」',answer:['：','！']},
+  {sentence:'閱讀圖書能增進知識___瀏覽互聯網能獲得最新資訊。',answer:['；']}
+];
